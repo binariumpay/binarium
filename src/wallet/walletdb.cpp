43f1d37e9e6c95bb6bd19c14923f187f6d7a383a
Realharon <haron@dcyber.ru>
2019-09-26 02:28:04 +0500

    [Sync] Apply patch 414272a

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -1094,7 +1094,7 @@ bool CWalletDB::Recover(CDBEnv& dbenv, const std::string& filename, bool fOnlyKe
     }
     LogPrintf("Salvage(aggressive) found %u records\n", salvagedData.size());
 
-    boost::scoped_ptr<Db> pdbCopy(new Db(dbenv.dbenv, 0));
+    std::unique_ptr<Db> pdbCopy(new Db(dbenv.dbenv, 0));
     int ret = pdbCopy->open(NULL,               // Txn pointer
                             filename.c_str(),   // Filename
                             "main",             // Logical db name
