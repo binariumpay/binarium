9f5de397da53eae9765d7d7495c8106b122ee3ab
binariumpay <a.novozhilov@gaijin.team>
2023-01-12 21:52:59 +0300

    DEP-224 Upgrade dependencies system

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,8 +1,8 @@
 package=expat
-$(package)_version=2.1.0
+$(package)_version=2.5.0
 $(package)_download_path=http://sourceforge.net/projects/expat/files/expat/$($(package)_version)
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=823705472f816df21c8f6aa026dd162b280806838bb55b3432b0fb1fcca7eb86
+$(package)_sha256_hash=6b902ab103843592be5e99504f846ec109c1abb692e85347587f237a4ffa1033
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-static
--- depends/packages/freetype.mk
+++ depends/packages/freetype.mk
@@ -1,11 +1,11 @@
 package=freetype
-$(package)_version=2.5.3
-$(package)_download_path=http://downloads.sourceforge.net/$(package)
-$(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=c0848b29d52ef3ca27ad92e08351f023c5e24ce8cea7d8fe69fc96358e65f75e
+$(package)_version=2.10.4
+$(package)_download_path=https://de.freedif.org/savannah/freetype/
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_sha256_hash=5eab795ebb23ac77001cfb68b7d4d50b5d6c7469247b0b01b2c953269f658dac
 
 define $(package)_set_vars
-  $(package)_config_opts=--without-zlib --without-png --disable-static
+  $(package)_config_opts=--without-zlib --without-png --disable-static --enable-freetype-config
   $(package)_config_opts_linux=--with-pic
 endef
 
